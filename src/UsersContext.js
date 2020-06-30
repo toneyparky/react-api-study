700738e5dd780624f7060168ed7e1021820d079f
toneyparky <qkrtmddhks95@gmail.com>
2020-06-30 21:54:10 +0900

    feat: context를 사용하기
    - 중복되는 부분 모듈화

---
+++ src/UsersContext.js
@@ -0,0 +1,61 @@
+import React, {createContext, useContext, useReducer} from 'react';
+import {createAsyncDispatcher, createAsyncHandler, initialAsyncState} from './asyncActionUtils';
+import * as api from './api';
+
+const initialState = {
+  users: initialAsyncState,
+  user: initialAsyncState
+};
+
+const usersHandler = createAsyncHandler('GET_USERS', 'users');
+const userHandler = createAsyncHandler('GET_USER', 'user');
+
+function usersReducer(state, action) {
+  switch (action.type) {
+    case 'GET_USERS':
+    case 'GET_USERS_SUCCESS':
+    case 'GET_USERS_ERROR':
+      return usersHandler(state, action);
+    case 'GET_USER':
+    case 'GET_USER_SUCCESS':
+    case 'GET_USER_ERROR':
+      return userHandler(state, action);
+    default:
+      throw new Error(`Unhanded action type: ${action.type}`);
+  }
+}
+
+// State 용 Context 와 Dispatch 용 Context 따로 만들어주기
+const UsersStateContext = createContext(null);
+const UsersDispatchContext = createContext(null);
+
+// 위에서 선언한 두가지 Context 들의 Provider 로 감싸주는 컴포넌트
+export function UsersProvider({children}) {
+  const [state, dispatch] = useReducer(usersReducer, initialState);
+  return (
+    <UsersStateContext.Provider value={state}>
+      <UsersDispatchContext.Provider value={dispatch}>
+        {children}
+      </UsersDispatchContext.Provider>
+    </UsersStateContext.Provider>
+  );
+}
+
+export function useUsersState() {
+  const state = useContext(UsersStateContext);
+  if (!state) {
+    throw new Error('Cannot find UsersProvider');
+  }
+  return state;
+}
+
+export function useUsersDispatch() {
+  const dispatch = useContext(UsersDispatchContext);
+  if (!dispatch) {
+    throw new Error('Cannot find UsersProvider');
+  }
+  return dispatch;
+}
+
+export const getUsers = createAsyncDispatcher('GET_USERS', api.getUsers);
+export const getUser = createAsyncDispatcher('GET_USER', api.getUser);
